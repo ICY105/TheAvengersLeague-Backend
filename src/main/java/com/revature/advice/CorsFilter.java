42dcb1118e8c56da3aa462da545a1350974d5d8a
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-16 16:18:13 -0400

    update login (again)

--- src/main/java/com/revature/advice/CorsFilter.java
+++ src/main/java/com/revature/advice/CorsFilter.java
@@ -1,6 +1,9 @@
 package com.revature.advice;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -18,19 +21,33 @@ import com.revature.controller.Visitor;
 @WebFilter("/*")
 public class CorsFilter extends OncePerRequestFilter {
 	
-    @Override
-    protected void doFilterInternal(final HttpServletRequest req, final HttpServletResponse resp, final FilterChain chain) throws ServletException, IOException {
-        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        resp.setHeader("Access-Control-Allow-Credentials", "true");
-        resp.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        resp.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        
-        final HttpSession session = req.getSession();
-        if(session != null && session.getAttribute("visitor") == null) {
-        	session.setAttribute("visitor", new Visitor());
-        }
-        
-        chain.doFilter(req, resp);
-    }
-    
+	private static final Map<String,Visitor> sessions = new HashMap<>();
+
+	@Override
+	protected void doFilterInternal(final HttpServletRequest req, final HttpServletResponse resp,
+			final FilterChain chain) throws ServletException, IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		resp.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+		resp.setHeader("Access-Control-Allow-Headers",
+				"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+		resp.setHeader("Access-Control-Expose-Headers", "Authorization");
+
+		final HttpSession session = req.getSession();
+		
+		final String token = req.getHeader("Authorization");
+		if(token == null || !sessions.containsKey(token)) {
+			final String newToken = UUID.randomUUID().toString();
+			final Visitor visitor = new Visitor();
+			sessions.put(newToken, visitor);
+			session.setAttribute("visitor", visitor);
+			resp.setHeader("Authorization", newToken);
+		} else {
+			session.setAttribute("visitor", sessions.get(token));
+			resp.setHeader("Authorization", token);
+		}
+
+		chain.doFilter(req, resp);
+	}
+
 }

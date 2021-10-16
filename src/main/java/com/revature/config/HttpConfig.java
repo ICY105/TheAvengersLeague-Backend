61f6add673cd404ec9a69d304615a9b9445510de
ImCoolYeah105 <maw23456789@gmail.com>
2021-10-15 20:57:47 -0400

    add SameSite=None hopefully

---
+++ src/main/java/com/revature/config/HttpConfig.java
@@ -0,0 +1,23 @@
+package com.revature.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class HttpConfig {
+	
+	private final ApplicationContext applicationContext;
+
+	@EventListener
+	public void onApplicationEvent(final ContextRefreshedEvent event) {
+		final DefaultCookieSerializer cookieSerializer = applicationContext.getBean(DefaultCookieSerializer.class);
+		System.out.println("Received DefaultCookieSerializer, Overriding SameSite Strict");
+		cookieSerializer.setSameSite("None");
+	}
+}
